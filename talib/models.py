dbaaffa9144064c4ec658f77e65505ff3158b907
Dases95 <dases@protonmail.com>
2018-07-06 09:50:30 +0100

    implementing user model

--- talib/models.py
+++ talib/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserProfileInfo(models.Model):
+    #create relationship
+    user = models.OneToOneField(User)
+    #add any additional attrebutes you want
+    country = models.CharField(blank=True)
+    def __str__(self):
+        return self.user.username
